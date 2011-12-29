fad58363835900dda1299c44864168dc9fc8fc0f
Nicolas Marchildon <nicolas@marchildon.net>
2011-12-28 23:24:48 -0500

    MD5 plugin checks md5 of file when sister filename.md5 exists.

---
+++ plugins/md5.rb
@@ -0,0 +1,16 @@
+require 'digest/md5'
+
+class RMagickPlugin
+  BadFiles.register_extensions /\.md5/i do |file|
+    expected = read_checksum(file)
+    if expected
+      actual_file = file[/(.*)\.md5/, 1]
+      actual = Digest::MD5.file(actual_file).hexdigest
+      raise "Invalid MD5 checksum: expected #{expected}, got #{actual} (#{actual_file})" if expected != actual
+    end
+  end
+
+  def self.read_checksum file
+    IO.readlines(file)[0].split[0]
+  end
+end
